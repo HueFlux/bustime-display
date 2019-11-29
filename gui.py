83ffad1d092ba8f53a1cf6aa009cba85f9a7a264
Larry <larry.vasquez@my.liu.edu>
2019-11-29 01:16:20 -0500

    Make line_name, destination_name and wait_time attributes of BusTimeWidget

--- gui.py
+++ gui.py
@@ -15,18 +15,23 @@ def toggle_fullscreen(self, event=None):
 root.bind('<Command-f>', toggle_fullscreen)
 
 class BusTimeWidget:
-    def __init__(self, frame):
+    def __init__(self, frame, line_name, destination_name, wait_time):
+
+        self.line_name = line_name
+        self.destination_name = destination_name
+        self.wait_time = wait_time
+
         self.frame = frame
         self.frame_button = tk.Button(self.frame, command=lambda: print("Button Pressed"))
         self.frame_button.place(relwidth=1, relheight=1)
 
-        bus_line_label = tk.Button(self.frame_button, text="Q39", font=('Roboto', 150, 'bold'), bg='#4d94ff')
+        bus_line_label = tk.Button(self.frame_button, text=line_name, font=('Roboto', 150, 'bold'), bg='#4d94ff')
         bus_line_label.place(relx=0.025, rely=0.25, relwidth=0.25, relheight=0.5)
 
-        destination_label = tk.Button(self.frame_button, text="LI CITY QUEENS PLZ", font=('Roboto', 60, 'bold'), relief='flat')
+        destination_label = tk.Button(self.frame_button, text=destination_name, font=('Roboto', 60, 'bold'), relief='flat')
         destination_label.place(relx=0.3, rely=0.25, relwidth=0.45, relheight=0.5)
 
-        wait_time_label = tk.Button(self.frame_button, text="15 min", font=('Roboto', 80, 'bold'), command=self.clear)
+        wait_time_label = tk.Button(self.frame_button, text=f"{wait_time} min", font=('Roboto', 80, 'bold'), command=self.clear)
         wait_time_label.place(relx=0.775, rely=0.25, relwidth=0.20, relheight=0.5)
 
     def clear(self):
@@ -39,11 +44,11 @@ canvas.pack()
 top_frame = tk.Frame(root, bd=5, bg=background_color)
 top_frame.place(relx=0, rely=0, relwidth=1, relheight=0.5)
 
-top_bus_widget = BusTimeWidget(top_frame)
+top_bus_widget = BusTimeWidget(top_frame, 'Q39', 'LI CITY QUEENS PLZ', 15)
 
 lower_frame = tk.Frame(root, bd=5, bg=background_color)
 lower_frame.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
 
-lower_bus_widget = BusTimeWidget(lower_frame)
+lower_bus_widget = BusTimeWidget(lower_frame, 'Q67', 'LI CITY QUEENS PLZ', 1)
 
 root.mainloop()
