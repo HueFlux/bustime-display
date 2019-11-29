3afd30092b11066ed04a6c6a1451825b92b2652b
Larry <larry.vasquez@my.liu.edu>
2019-11-29 01:07:30 -0500

    Change BusWidget class name to BusTimeWidget Add clear() method to BusTimeWidget

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ def toggle_fullscreen(self, event=None):
 
 root.bind('<Command-f>', toggle_fullscreen)
 
-class BusWidget:
+class BusTimeWidget:
     def __init__(self, frame):
         self.frame = frame
         self.frame_button = tk.Button(self.frame, command=lambda: print("Button Pressed"))
@@ -26,8 +26,12 @@ class BusWidget:
         destination_label = tk.Button(self.frame_button, text="LI CITY QUEENS PLZ", font=('Roboto', 60, 'bold'), relief='flat')
         destination_label.place(relx=0.3, rely=0.25, relwidth=0.45, relheight=0.5)
 
-        wait_time_label = tk.Button(self.frame_button, text="15 min", font=('Roboto', 80, 'bold'))
+        wait_time_label = tk.Button(self.frame_button, text="15 min", font=('Roboto', 80, 'bold'), command=self.clear)
         wait_time_label.place(relx=0.775, rely=0.25, relwidth=0.20, relheight=0.5)
+
+    def clear(self):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
 
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg=background_color)
 canvas.pack()
@@ -35,11 +39,11 @@ canvas.pack()
 top_frame = tk.Frame(root, bd=5, bg=background_color)
 top_frame.place(relx=0, rely=0, relwidth=1, relheight=0.5)
 
-top_bus_widget = BusWidget(top_frame)
+top_bus_widget = BusTimeWidget(top_frame)
 
 lower_frame = tk.Frame(root, bd=5, bg=background_color)
 lower_frame.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
 
-lower_bus_widget = BusWidget(lower_frame)
+lower_bus_widget = BusTimeWidget(lower_frame)
 
 root.mainloop()
