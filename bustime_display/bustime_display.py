45f0aec0b05e2be87fa29424c0e4ecdf6f500044
Larry <larry.vasquez@my.liu.edu>
2020-04-13 02:49:36 -0400

    Update fixtures in new update_bus_times() method

--- bustime_display/bustime_display.py
+++ bustime_display/bustime_display.py
@@ -6,6 +6,9 @@ from os import environ
 from twilio.rest import Client
 
 class BusTimeApp:
+    """
+    A class used to contain the bustime-display application.
+    """
 
     Notification = namedtuple('Notification', 'vehicle_ref, wait_time')
 
@@ -15,7 +18,7 @@ class BusTimeApp:
 
         self.is_fullscreen = False
         self.master.bind('<Control-f>', self.toggle_fullscreen)
-
+        # Use width and height of the screen for sizing the app window
         self.WIDTH = root.winfo_screenwidth()
         self.HEIGHT = root.winfo_screenheight()
 
@@ -23,35 +26,34 @@ class BusTimeApp:
 
         self.canvas = tk.Canvas(self.master, width=self.WIDTH, height=self.HEIGHT, bg=self.background_color)
         self.canvas.pack()
-
+        # Frame to contain the upper_bus_widget
         self.upper_frame = tk.Frame(self.master, bd=5, bg=self.background_color)
         self.upper_frame.place(relx=0, rely=0, relwidth=1, relheight=0.5)
-
+        # Frame to contain the lower_bus_widget
         self.lower_frame = tk.Frame(self.master, bd=5, bg=self.background_color)
         self.lower_frame.place(relx=0, rely=0.5, relwidth=1, relheight=0.5)
 
-        self.fixtures = []
-        self.lower_bus_time = 1 # Index of bus time displayed in lower_frame
+        self.fixtures = [] # List to hold BusTime namedtuples
+        self.lower_bus_time = 1 # Index of BusTime displayed in lower_frame
 
+        # Get Twilio account credentials and phone number from local environment
         self.account_sid = environ['TWILIO_ACCOUNT_SID']
         self.auth_token = environ['TWILIO_AUTH_TOKEN']
         self.client = Client(self.account_sid, self.auth_token)
-        self.twilio_phone = '+12055513213'
-
+        self.twilio_phone = environ['TWILIO_PHONE_NUMBER']
+        # Get user's phone number from local environment
         self.phone_number = environ['MY_PHONE_NUMBER']
 
+        # Notification namedtuple used to hold notification data
         self.notification = None
 
+        # Populate upper_frame and lower_frame with BusTimeWidgets
         self.display_bus_times()
 
     def display_bus_times(self):
-        q39_fixtures = bustime.bus_times(503991, dump_json=True)
-        q67_fixtures = bustime.bus_times(505168, 'MTABC_Q67')
+        self.update_bus_times() # Update fixtures
 
-        self.fixtures = sorted(q39_fixtures + q67_fixtures,
-                               key=attrgetter('estimated_wait_time'))
-
-        self.clear_bus_times()
+        self.clear_bus_times() # Clear upper_frame and lower_frame
 
         if self.fixtures:
             self.upper_bus_widget = self.BusTimeWidget(self, self.upper_frame,
@@ -75,12 +77,34 @@ class BusTimeApp:
 
         self.master.after(5000, self.display_bus_times)
 
+    def update_bus_times(self):
+        q39_fixtures = bustime.bus_times(503991, dump_json=True)
+        q67_fixtures = bustime.bus_times(505168, 'MTABC_Q67')
+
+        self.fixtures = sorted(q39_fixtures + q67_fixtures,
+                               key=attrgetter('estimated_wait_time'))
+
     def set_notification(self, vehicle_ref, wait_time=5):
+        """
+        Creates Notification namedtuple and initiates notification checking.
+
+        Args:
+            vehicle_ref (str): The vehicle identifier.
+            wait_time (int, optional): The bus wait time, in minutes, at
+                which to send out the notification.
+
+        Returns:
+            None.
+        """
         self.notification = self.Notification(vehicle_ref, wait_time)
         print("Notification set.")
         self.notify()
 
     def notify(self):
+        """
+        Sends out notification when the bus in the Notification namedtuple has
+        reached the desired wait time, otherwise checks again in 5 seconds.
+        """
         for bus_time in self.fixtures:
             if (self.notification.vehicle_ref == bus_time.vehicle_ref and
                     self.notification.wait_time >= bus_time.estimated_wait_time):
@@ -114,6 +138,10 @@ class BusTimeApp:
         return "break"
 
     class BusTimeWidget:
+        """
+        Widget to display information for a single bus.
+        """
+
         def __init__(self, bustime_app, frame, bus_time, position):
 
             self.bustime_app = bustime_app
